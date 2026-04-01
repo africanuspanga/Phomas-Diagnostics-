8860dd91b3ad52cb99760bdec9fe4d00c5815359
Africanus Panga <africanuspanga@gmail.com>
2026-04-01 12:32:25 +0300

    Upgrade to Next.js 16.2

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+  {
+    files: ["tailwind.config.ts"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+])
+
+export default eslintConfig
